1b9998faf6dd6cdbb7254b3156dd603b0229cda8
temmip88 <82580573+temmip88@users.noreply.github.com>
2021-05-25 11:07:34 +0300

    Create extends2.py

---
+++ tiistai/extensions/extends2.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import sys
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("base2.html")
+
+#@app.route("/foo")
+#def foo():
+#    return render_template("foo.html")
+
+app.run()
