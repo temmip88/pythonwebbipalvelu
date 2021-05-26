e070f0b80870d967060b74104525c198d8a3c9c1
temmip88 <82580573+temmip88@users.noreply.github.com>
2021-05-26 12:52:40 +0300

    Update based.py

--- keskiviikko/databases/based.py
+++ keskiviikko/databases/based.py
@@ -1,23 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms.ext.sqlalchemy.orm import model_form
+
 
 app = Flask(__name__)
+app.secret_key = "Ib0iT5ieguxoof0Xu5hi4Neengosai"
 db = SQLAlchemy(app)
 
 class Sammakot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
+    country = db.Column(db.String, nullable=False)
+    year = db.Column(db.String, nullable=False)
+
+
+CommentForm = model_form(Sammakot, base_class=FlaskForm, db_session=db.session)
 
 @app.before_first_request
-def initDB():
+def initMe():
     db.create_all()
 
-    db.session.add(Sammakot(name = "Apustaja"))
-    db.session.add(Sammakot(name = "Lättä"))
-    db.session.add(Sammakot(name = "Pepe"))
+    db.session.add(Sammakot(name = "Apustaja", country = "Fin", year = "2010"))
+    db.session.add(Sammakot(name = "Lättä", country = "Fin", year = "2011"))
+    db.session.add(Sammakot(name = "Pepe", country = "US", year = "2007"))
 
     db.session.commit()
 
+
+@app.route("/new", methods=["GET", "POST"])
+def addForm():
+    form = CommentForm()
+    return render_template("new.html", form = form)
 
 
 @app.route("/")
