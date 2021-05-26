e495326c4d1da1ca9beff17ab054588431c20d6e
temmip88 <82580573+temmip88@users.noreply.github.com>
2021-05-26 14:26:30 +0300

    Update based.py

--- keskiviikko/databases/based.py
+++ keskiviikko/databases/based.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms.ext.sqlalchemy.orm import model_form
